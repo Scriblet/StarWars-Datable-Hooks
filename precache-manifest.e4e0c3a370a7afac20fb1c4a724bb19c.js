e73f0e9c9fb9c3dfbbb8bd955808b7685348b221
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2021-02-21 06:23:17 -0300

    Updates

---
+++ precache-manifest.e4e0c3a370a7afac20fb1c4a724bb19c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b55bad7dbfe2715ec557e27ba85ee5",
+    "url": "/StarWars-Datable-Hooks/index.html"
+  },
+  {
+    "revision": "b0567f30059c5ba9cc31",
+    "url": "/StarWars-Datable-Hooks/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "7d1cc58078801bba4d74",
+    "url": "/StarWars-Datable-Hooks/static/css/main.d19b74a5.chunk.css"
+  },
+  {
+    "revision": "b0567f30059c5ba9cc31",
+    "url": "/StarWars-Datable-Hooks/static/js/2.eec92d6f.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/StarWars-Datable-Hooks/static/js/2.eec92d6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d1cc58078801bba4d74",
+    "url": "/StarWars-Datable-Hooks/static/js/main.62258536.chunk.js"
+  },
+  {
+    "revision": "b3fdad8a4d1ab4615a88",
+    "url": "/StarWars-Datable-Hooks/static/js/runtime-main.0d29273f.js"
+  }
+]);
